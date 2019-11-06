7f486c3d522b003e5e850e705656c302bbeec0ec
ZarebskiDavid <zarebskidavid@gmail.com>
2019-11-06 19:07:58 +0100

    fix wrong filters for nginx

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -19,42 +19,28 @@ rm /etc/fail2ban/jail.d/alpine-ssh.conf
 
 touch /etc/fail2ban/jail.d/nginx.conf
 printf '%s\n' \
+   "[nginx-badbots]" \
+   "enabled  = true" \
+   "port     = http,https" \
+   "filter   = nginx-botsearch" \
+   "logpath  = /var/log/nginx/*access.log" \
+   "maxretry = 2" \
+   "findtime = 300" \
+   " " \
    "[nginx-http-auth]" \
    "enabled  = true" \
    "port     = http,https" \
    "filter   = nginx-http-auth" \
-   "logpath  = /var/log/nginx/error.log" \
+   "logpath  = /var/log/nginx/*error.log" \
    "maxretry = 6" \
+   "findtime = 300" \
    " " \
-   "[nginx-noscript]" \
-   " " \
+   "[nginx-conn-limit]" \
    "enabled  = true" \
+   "filter   = nginx-limit-req" \
    "port     = http,https" \
-   "filter   = nginx-noscript" \
-   "logpath  = /var/log/nginx/access.log" \
-   "maxretry = 6" \
-   " " \
-   "[nginx-badbots]" \
-   " " \
-   "enabled  = true" \
-   "port     = http,https" \
-   "filter   = nginx-badbots" \
-   "logpath  = /var/log/nginx/access.log" \
-   "maxretry = 2" \
-   " " \
-   "[nginx-nohome]" \
-   "enabled  = true" \
-   "port     = http,https" \
-   "filter   = nginx-nohome" \
-   "logpath  = /var/log/nginx/access.log" \
-   "maxretry = 2" \
-   " " \
-   "[nginx-noproxy]" \
-   "enabled  = true" \
-   "port     = http,https" \
-   "filter   = nginx-noproxy" \
-   "logpath  = /var/log/nginx/access.log" \
-   "maxretry = 2" \
+   "logpath  = /var/log/nginx/*error.log" \
+   "findtime = 300" \
    " " \
    "[app-authentification]" \
    "enabled  = true" \
@@ -62,9 +48,13 @@ printf '%s\n' \
    "bantime  = 86400" \
    "port     = http,https" \
    "filter   = auth-app" \
-   "logpath  = /var/log/nginx/access.log" \
+   "logpath  = /var/log/nginx/*access.log" \
    "maxretry = 3" \
 >> /etc/fail2ban/jail.d/nginx.conf
+
+
+
+
 
 # EDIT nginx.conf with CSP
 export DOMAIN_NAME
